e1d0a788b233e6b815fa2da521800576fec69a68
cortiz-maker <cortiz@quantrex.cl>
2026-05-26 18:27:09 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const axios = require("axios");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
+app.options("*", cors());
 
 const DT_API_URL = "https://activationcode.dispatchtrack.com/api/external/v1";
 const DT_API_KEY = process.env.DISPATCHTRACK_API_KEY;
